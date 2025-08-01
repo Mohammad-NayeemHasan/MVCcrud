a592d2e5456c1f4eaec2deb0855bf43e347b1230
Nayeem <mdnayeemhasan7>
2025-08-01 19:05:27 +0600

    Add project files.

---
+++ MVCcrud/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCcrud.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string Name { get;set; } 
+        public decimal Salary { get; set; }
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
---
+++ MVCcrud/Models/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCcrud.Models
+{
+    public class EmployeeDbContext: DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+    
+}
